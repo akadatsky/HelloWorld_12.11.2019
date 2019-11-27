50b1e6c5a2f0e5ca96d972ee2b5f4b621720cb61
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-11-27 18:55:11 +0200

    Added test

--- src/com/akadatsky/Hello.java
+++ src/com/akadatsky/Hello.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Hello {
 
+    // test
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(arr));
