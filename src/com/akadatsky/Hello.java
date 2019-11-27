884c1123e789eabce07a0a78ba5dda32491ac537
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-11-27 18:37:33 +0200

    Initial commit

---
+++ src/com/akadatsky/Hello.java
@@ -0,0 +1,25 @@
+package com.akadatsky;
+
+import java.util.Arrays;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr);
+        changeArray(arr);
+        System.out.println("еще раз в main arr: " + Arrays.toString(arr));
+        System.out.println(arr);
+    }
+
+    private static void changeArray(int[] arr) {
+        arr[0] = 55;
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        arr = arr2;
+        System.out.println("Новый массив: " + Arrays.toString(arr));
+        System.out.println(arr);
+    }
+
+}
+
---
+++ src/com/akadatsky/other/Util.java
@@ -0,0 +1,21 @@
+package com.akadatsky.other;
+
+import javax.swing.text.Document;
+
+public class Util {
+
+    public static void sayHello() {
+        System.out.println("test");
+    }
+
+    /**
+     * sum of tho values
+     * @param a
+     * @param b
+     * @return sum
+     */
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+}
